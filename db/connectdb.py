af295cd38a813f1042791b1a9ca48abd9fdea3f7
franklin <xingnan.lin@columbia.edu>
2023-11-19 17:50:52 -0500

    #implement 4 core operations for microservice-2

--- db/connectdb.py
+++ db/connectdb.py
@@ -1,4 +1,5 @@
 import psycopg2
+import uuid
 
 def connect():
     conn=psycopg2.connect(
@@ -13,17 +14,36 @@ def connect():
 conn,cur=connect()
 
 #create a table
-#command="""CREATE TABLE IF NOT EXISTS subscriptions (subscription_id VARCHAR(255) PRIMARY KEY, analyst_id VARCHAR(255), report_id VARCHAR(255), feedbacks VarChar(255), notifications VarChar(255), activity VarChar(255))"""
-#cur.execute(command)
-#conn.commit()
+# command="""CREATE TABLE IF NOT EXISTS subscriptions (subscription_id VARCHAR(255) PRIMARY KEY, subscriber_id VARCHAR(255), analyst_id VARCHAR(255), report_id VARCHAR(255), feedbacks VarChar(255), notifications VarChar(255), activity VarChar(255))"""
+# cur.execute(command)
+# conn.commit()
 
 #insert data
-#command="""INSERT INTO subscriptions (subscription_id, analyst_id, report_id, feedbacks, notifications, activity) VALUES (%s, %s, %s, %s, %s, %s)"""
-#cur.execute(command,("4","4","4","feedbacks","notifications","activity"))
-#conn.commit()
+# subscription_id="1"
+# subscriber_id="1"
+# analyst_id="1"
+# report_id="1"
+# feedbacks="feedbacks: None"
+# notifications="notifications: None"
+# activity="activity: None"
+
+# command="""INSERT INTO subscriptions (subscription_id, subscriber_id, analyst_id, report_id, feedbacks, notifications, activity) VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+# cur.execute(command,(subscription_id, subscriber_id, analyst_id, report_id, feedbacks, notifications, activity))
+# conn.commit()
 
 #fetch database
-#command="""SELECT * FROM subscriptions"""
-#cur.execute(command)
-#result=cur.fetchall()
-#print(result)
+# command="""SELECT * FROM subscriptions"""
+# cur.execute(command)
+# result=cur.fetchall()
+# print(result)
+
+#delete data
+# command="""DELETE FROM subscriptions WHERE subscription_id=%s"""
+# cur.execute(command,("1"))
+# conn.commit()
+
+#delete table
+# command="""DROP TABLE subscriptions"""
+# cur.execute(command)
+# conn.commit()
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Body, status
 import uvicorn
 from resources.students import StudentsResource
 from db.connectdb import connect
+import uuid
 
 app = FastAPI()
 
@@ -26,31 +27,98 @@ async def get_subscription_id(subscription_id: str):
     conn,cur=connect()
     cur.execute(command,(subscription_id))
     result=cur.fetchall()
-    return [{"subscription_id":row[0]} for row in result]
+    return [{row} for row in result]
 
 @app.get("/subscription/{subscription_id}/subscriber")
 async def subscriber(subscription_id: str):
-    return {"GET the subscriber specific to unique subscription id"}
+    command="SELECT * FROM subscriptions WHERE subscription_id=%s"
+    conn,cur=connect()
+    cur.execute(command,(subscription_id))
+    result=cur.fetchall()
+    return [{"subscriber_id":row[1]} for row in result]
 
 @app.get("/subscription/{subscription_id}/analyst")
 async def analyst(subscription_id: str):
-    return {"GET the analyst specific to unique subscription id"}
+    command="SELECT * FROM subscriptions WHERE subscription_id=%s"
+    conn,cur=connect()
+    cur.execute(command,(subscription_id))
+    result=cur.fetchall()
+    return [{"subscriber_id":row[2]} for row in result]
 
 @app.get("/subscription/{subscription_id}/report")
 async def report(subscription_id: str):
-    return {"GET the report specific to unique subscription id"}
+    command="SELECT * FROM subscriptions WHERE subscription_id=%s"
+    conn,cur=connect()
+    cur.execute(command,(subscription_id))
+    result=cur.fetchall()
+    return [{"subscriber_id":row[3]} for row in result]
 
-@app.get("/subscription/{subscription_id}/report/feedback")
+@app.get("/subscription/{subscription_id}/feedback")
 async def report_feedback(subscription_id: str):
-    return {"GET feedbacks for reports specific to unique subscription id"}
+    command="SELECT * FROM subscriptions WHERE subscription_id=%s"
+    conn,cur=connect()
+    cur.execute(command,(subscription_id))
+    result=cur.fetchall()
+    return [{"subscriber_id":row[4]} for row in result]
 
 @app.get("/subscription/{subscription_id}/notifications")
 async def notification(subscription_id: str):
-    return {"GET list of notification preferences specific to unique subscription id"}
+    command="SELECT * FROM subscriptions WHERE subscription_id=%s"
+    conn,cur=connect()
+    cur.execute(command,(subscription_id))
+    result=cur.fetchall()
+    return [{"subscriber_id":row[5]} for row in result]
 
 @app.get("/subscription/{subscription_id}/activity")
 async def activity(subscription_id: str):
-    return {"GET activity logs or history specific to unique subscription id"}
+    command="SELECT * FROM subscriptions WHERE subscription_id=%s"
+    conn,cur=connect()
+    cur.execute(command,(subscription_id))
+    result=cur.fetchall()
+    return [{"subscriber_id":row[6]} for row in result]
+
+#create new subscription
+@app.post("/subscription/")
+async def create_subscription(body=Body(...)):
+    subscription_id=body["subscription_id"]
+    subscriber_id=body["subscriber_id"]
+    analyst_id=body["analyst_id"]
+    report_id=body["report_id"]
+    feedbacks=body["feedbacks"]
+    notifications=body["notifications"]
+    activity=body["activity"]
+
+    command="""INSERT INTO subscriptions (subscription_id, subscriber_id, analyst_id, report_id, feedbacks, notifications, activity) VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+    conn,cur=connect()
+    cur.execute(command,(subscription_id, subscriber_id, analyst_id, report_id, feedbacks, notifications, activity))
+    conn.commit()
+    return {"Updated": body}
+
+#delete subscription basted on subscription id
+@app.delete("/subscription/{subscription_id}")
+async def delete_subscription(subscription_id: str):
+    command="""DELETE FROM subscriptions WHERE subscription_id=%s"""
+    conn,cur=connect()
+    cur.execute(command,(subscription_id))
+    conn.commit()
+    return {"message": "subscription deleted"}
+
+#update database based on subscription id
+@app.put("/subscription/")
+async def update_subscription(body=Body(...)):
+    subscription_id=body["subscription_id"]
+    subscriber_id=body["subscriber_id"]
+    analyst_id=body["analyst_id"]
+    report_id=body["report_id"]
+    feedbacks=body["feedbacks"]
+    notifications=body["notifications"]
+    activity=body["activity"]
+
+    command="""UPDATE subscriptions SET subscriber_id=%s, analyst_id=%s, report_id=%s, feedbacks=%s, notifications=%s, activity=%s WHERE subscription_id=%s"""
+    conn,cur=connect()
+    cur.execute(command,(subscriber_id, analyst_id, report_id, feedbacks, notifications, activity, subscription_id))
+    conn.commit()
+    return {"Updated": body}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8012)
